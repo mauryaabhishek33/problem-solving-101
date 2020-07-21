518b02575d745de3408bbc86845c4d88526d0891
Abhishek Maurya <mauryaabhishek33@gmail.com>
2020-07-21 07:01:46 +0530

    Accessing elements in an array

--- problem-solving-101/Categories/arrays/DVD.java
+++ problem-solving-101/Categories/arrays/DVD.java
@@ -65,5 +65,42 @@ public class DVD {
 			// The Avengers, directed by Joss Whedon, released in 2012
 			// null
 			// Star Wars, directed by George Lucas, released in 1977
+			
+			//Writing Items into an Array with a Loop
+			int[] squareNumbers = new int[10];
+
+			// Go through each of the Array indexes, from 0 to 9.
+			for (int i = 0; i < 10; i++) {
+			    // We need to be careful with the 0-indexing. The next square number
+			    // is given by (i + 1) * (i + 1).
+			    // Calculate it and insert it into the Array at index i.
+			    int square = (i + 1) * (i + 1);
+			    squareNumbers[i] = square;
+			}
+			//Reading Items from an Array with a Loop
+			// Go through each of the Array indexes, from 0 to 9.
+			for (int i = 0; i < 10; i++) {
+			    // Access and print what's at the i'th index.
+			    System.out.println(squareNumbers[i]);
+			}
+
+			// Will print:
+			// 1
+			// 4
+			// 9
+			// 16
+			// 25
+			// 36
+			// 49
+			// 64
+			// 81
+			// 100
+			// For each VALUE in the Array.
+			for (int square : squareNumbers) {
+			    // Print the current value of square.
+			    System.out.println(square);
+			}
+			// Prints exactly the same as the previous example.
+
 		}
 }
